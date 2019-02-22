d06a3a6a59d79c93a58314e145899db0b33fa145
Pere Lluis Huguet Cabot <phuguetcabot@amsterdamdatacollective.com>
2019-02-23 00:14:27 +0100

    changes

--- parser.py
+++ parser.py
@@ -1,18 +1,20 @@
+import os
+import sys
 def parse_rules(filename):
     clauses = []
-    units = set()
+    variables = set()
     for clause in open(filename):
         if clause.startswith('c'):
             continue
         if clause.startswith('p'):
-            n_units = clause.split()[2]
+            n_variables = clause.split()[2]
             continue
         clause_el = []
         for x in clause[:-2].split():
             clause_el.append(int(x))
-            units.add(abs(int(x)))
+            variables.add(abs(int(x)))
         clauses.append(clause_el)
-    return clauses, len(units), units
+    return clauses, len(variables), variables
 
 #we can take the literals while we parse the problem.
 
@@ -21,3 +23,7 @@ def parse_sudoku(filename):
     for clause in open(filename):
         units[clause[:-2]] = clause[-2]
     return units
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    parse_rules(filename)
