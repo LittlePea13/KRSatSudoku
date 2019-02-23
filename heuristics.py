1eb67e0c84b10f2fb28e71fbcf41e06d9fa464d8
tilla <szatidani@gmail.com>
2019-02-23 20:44:41 +0100

    jeroslow-wang heur

--- heuristics.py
+++ heuristics.py
@@ -10,3 +10,14 @@ def DLCS(clauses):
     return prob_dist.generate()
 def Random_split(clauses):
     return abs(random.choice([x for x in itertools.chain.from_iterable(clauses)]))
+
+def Jeroslow_wang(clauses):
+    jeroslow_values = {}
+    for clause in clauses:
+        for literal in clause:
+            if literal in jeroslow_values:
+                jeroslow_values[literal] += pow(2,-len(clause))
+            else:
+                jeroslow_values[literal] = pow(2,-len(clause))
+
+    return max(jeroslow_values, key=jeroslow_values.get
