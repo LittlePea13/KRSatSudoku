5c8c6a64c9e8f8feb408eb97c0f10975beea6a09
tilla <szatidani@gmail.com>
2019-02-24 01:01:28 +0100

    basic stat collector

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from simplificator import update_clauses, simplification, simplification_sudoku,
 from heuristics import Random_split, DLCS
 from sanity import print_sudoku, check_sudoku
 from parser import parse_rules
+from statistics import StatCollector
+
 import time
 import sys
 
-def DL(clauses, variables, n_iterations, heuristics = Random_split):
+def DL(clauses, variables, n_iterations, stat_collector, heuristics = Random_split):
     '''
         Recursive Solver
     '''
@@ -14,28 +16,44 @@ def DL(clauses, variables, n_iterations, heuristics = Random_split):
         return True, variables
     for clause in clauses:
         if clause == []:
+            #INC backtrack
+            #stat_collector.n_backtrack += 1
             return False, variables
+        
     new_clauses, variables, sat = simplification(clauses, variables)
     new_clauses = update_clauses(new_clauses, variables)
+    
+    #RECALC clause and variable stat
+    stat_collector.update_stats(clauses, variables)
+    
     if len([1 for element in clauses if len(element)==1])>1:
-        return DL(new_clauses, variables, n_iterations, heuristics)
+        return DL(new_clauses, variables, n_iterations, stat_collector, heuristics)
+    
     split = heuristics(new_clauses)
     value = 1 if random.random() < 0.5 else -1
-    sat, variables_split = DL(update_clauses(new_clauses, {**variables,**{split:value}}), {**variables,**{split:value}}, n_iterations +1, heuristics)
+    #value = 1
+    #INC split
+    stat_collector.n_split += 1
+    sat, variables_split = DL(update_clauses(new_clauses, {**variables,**{split:value}}), {**variables,**{split:value}}, n_iterations +1, stat_collector,heuristics)
+    
     if sat is False:
-        sat, variables_split = DL(update_clauses(new_clauses, {**variables,**{split:-value}}), {**variables,**{split:-value}}, n_iterations +1, heuristics)
+        #INC flip
+        #stat_collector.n_flip += 1
+        sat, variables_split = DL(update_clauses(new_clauses, {**variables,**{split:-value}}), {**variables,**{split:-value}}, n_iterations +1, stat_collector, heuristics)
+    
     return sat, variables_split
-
 def main(filename, heuristics):
     rules,_,_ = parse_rules('sudoku-rules.txt')
     sudoku_clauses,_,_ = parse_rules(filename)
     clauses = sudoku_clauses + rules
     t0 = time.time()
-    result, variables = DL(clauses, {}, 1)
+    stat_collector = StatCollector()
+    result, variables = DL(clauses, {}, 1, stat_collector)
     t1 = time.time()
     print('Time to solve sudoku:', t1-t0)
     print('Result ', result)
     print_sudoku(sorted([k for k,v in variables.items() if v>0]))
+    stat_collector.print_stats()
     return result
 
 if __name__ == '__main__':
