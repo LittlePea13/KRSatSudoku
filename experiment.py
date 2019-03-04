8fa20f87e15a8dc4854ff0e937f60c6fa0888956
tilla <szatidani@gmail.com>
2019-03-04 01:28:25 +0100

    fix experiments to run with new refact

--- experiment.py
+++ experiment.py
@@ -1,5 +1,4 @@
-from heuristics import Random_split, DLCS, Jeroslow_wang, Row_wise_lenght, Column_wise_lenght, Block_wise_lenght, Row_wise_rand, \
-    Column_wise_rand, Block_wise_rand, Row_wise, Column_wise, Block_wise
+import heuristics
 from dp import davis_putnam
 import sys
 import os
@@ -7,33 +6,8 @@ import pandas as pd
 
 
 def experiment(sudoku_path, heur, checkpoint = True):
-    '''if heur == 'DLCS' or heur is None:
-        heuristics = DLCS
-    elif heur == 'Jeroslow_wang':
-        heuristics = Jeroslow_wang
-    elif heur == 'Row_wise_lenght':
-        heuristics = Row_wise_lenght
-    elif heur == 'Column_wise_lenght':
-        heuristics = Column_wise_lenght
-    elif heur == 'Block_wise_lenght':
-        heuristics = Block_wise_lenght
-    elif heur == 'Block_wise':
-        heuristics = Block_wise
-    elif heur == 'Row_wise':
-        heuristics = Row_wise
-    elif heur == 'Column_wise':
-        heuristics = Column_wise
-    elif heur == 'Block_wise_rand':
-        heuristics = Block_wise_rand
-    elif heur == 'Row_wise_rand':
-        heuristics = Row_wise_rand
-    elif heur == 'Column_wise_rand':
-        heuristics = Column_wise_rand
-    elif heur == 'Random_split':
-        heuristics = Random_split
-    else:
-        print('wrong heuristics')
-'''
+
+    heuristics_func = getattr(heuristics, heur)
 
     print(sudoku_path)
     sudoku_list = os.listdir(sudoku_path)
@@ -42,26 +16,26 @@ def experiment(sudoku_path, heur, checkpoint = True):
     result_df = pd.DataFrame(columns=['satisfied', 'time', 'n_iter', 'n_split', 'n_backtrack', 'vis_row', 'vis_column', 'vis_block', 'max_depth' ,'avg_depth'])
  
     if checkpoint == True:
-        starting_point = find_latest_checkpoint(heur.__name__)
+        starting_point = find_latest_checkpoint(heuristics_func.__name__)
         if starting_point != 0:
-            result_df = pd.read_csv('{}.result.checkpoint_{}.csv'.format(heur.__name__,starting_point),index_col='Unnamed: 0')
-        print('{} is starting at {}'.format(heur.__name__, starting_point+1))
+            result_df = pd.read_csv('{}.result.checkpoint_{}.csv'.format(heuristics_func.__name__,starting_point),index_col='Unnamed: 0')
+        print('{} is starting at {}'.format(heuristics_func.__name__, starting_point+1))
         
     for idx, filename in enumerate(sudoku_list[starting_point:],starting_point):
         sudoku_file = sudoku_path + filename
 
-        sat_result, time, stat_collector = davis_putnam(sudoku_file, heur, print_results=False)
+        sat_result, time, stat_collector = davis_putnam(sudoku_file, heuristics_func, print_results=False)
         stats = stat_collector.get_results()
         result_list = [sat_result, time] + list(stats)
         result_df = result_df.append(pd.Series(result_list, index=result_df.columns ), ignore_index=True)
 
         if (idx % int(all_len*0.1)) == 0 and idx > 0:
-            print('{} is done with {}/{}'.format(heur.__name__,idx+1, all_len))
+            print('{} is done with {}/{}'.format(heuristics_func.__name__,idx+1, all_len))
             if checkpoint == True:
-                result_df.to_csv('{}.result.checkpoint_{}.csv'.format(heur.__name__,idx))
+                result_df.to_csv('{}.result.checkpoint_{}.csv'.format(heuristics_func.__name__,idx))
 
-    print('{} is done'.format(heur.__name__))
-    result_df.to_csv('{}.result.csv'.format(heur.__name__))
+    print('{} is done'.format(heuristics_func.__name__))
+    result_df.to_csv('{}.result.csv'.format(heuristics_func.__name__))
 
 
 def find_latest_checkpoint(heur):
--- run_sudoku_experiments.py
+++ run_sudoku_experiments.py
@@ -1,6 +1,5 @@
 from experiment import experiment
 from multiprocessing import Pool
-import heuristics
 
 heurs = ['Random_split', 'DLCS', 'Jeroslow_wang', 'Row_wise_lenght', 'Column_wise_lenght', 'Block_wise_lenght', 'Row_wise_rand', \
     'Column_wise_rand', 'Block_wise_rand', 'Row_wise', 'Column_wise', 'Block_wise']
